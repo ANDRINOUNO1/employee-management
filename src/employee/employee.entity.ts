42089b2800efdf167a6501de78518dc763a5f060
ANDRINOUNO1 <mataandrewczar@gmail.com>
2025-03-07 13:10:45 +0800

    folders

---
+++ src/employee/employee.entity.ts
@@ -0,0 +1,37 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Employee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  position: string;
+
+  @Column({ nullable: true })
+  salary: number;
+
+  @ManyToOne(() => Department)
+  department: Department;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  hireDate: Date;
+}
+
+@Entity()
+export class Department {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @OneToMany(() => Employee, (employee) => employee.department)
+  employees: Employee[];
+}
